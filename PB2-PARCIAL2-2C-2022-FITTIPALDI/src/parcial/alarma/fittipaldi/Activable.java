4b90d353d2cee08c73d2c2c61df1e3193133a958
Fittipaldih <fittipaldi.h@gmail.com>
2022-11-09 08:49:26 -0300

    sigo creando el proyecto

---
+++ PB2-PARCIAL2-2C-2022-FITTIPALDI/src/parcial/alarma/fittipaldi/Activable.java
@@ -0,0 +1,5 @@
+package parcial.alarma.fittipaldi;
+
+public interface Activable {
+
+}
---
+++ PB2-PARCIAL2-2C-2022-FITTIPALDI/src/parcial/alarma/fittipaldi/UsuarioActivador.java
@@ -0,0 +1,5 @@
+package parcial.alarma.fittipaldi;
+
+public class UsuarioActivador extends Usuario implements Activable{
+
+}
